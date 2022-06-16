339e1f42e86aa737e56561658fbff8a2c20c0112
ombabadugunda <m.kryzh@gmail.com>
2022-06-16 21:50:46 +0300

    Update Lesson_2.sol

--- Lesson_2.sol
+++ Lesson_2.sol
@@ -4,23 +4,27 @@ pragma solidity 0.8.14;
 
 contract Banker {
     
-    mapping(address => uint) loans;
-    address owner;
+    mapping(address => uint) public loans;
+    address public owner;
 
-    constructor() {
+    constructor() payable {
         owner = msg.sender;
     }
+    
+    event Event(address client);
 
     receive() external payable {
+        pay();
     }
 
-    function fillBalance() public payable {
-        require(msg.sender == owner, "Only owner can perform this action");
+    function pay() public payable {
+        emit Event(msg.sender);
     }
 
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
+    
     function _checkAccount(address client, uint amount) internal view returns(bool) {
         return (loans[client] == amount);
     }
@@ -28,12 +32,7 @@ contract Banker {
     function lend(uint amount) public {
         require(getBalance() >= amount, "Contract doesn`t have enough money");
         address payable client = payable(msg.sender);
-        loans[client] = amount;
-        if (_checkAccount(client, amount)) {
-            client.transfer(amount);
-        } else {
-            revert();
-        }
+        loans[client] += amount;
     }
 
     function payback() public payable{
